4afc5947571dcef3556a74eaf1602ec447b29e13
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-18 23:44:59 +0200

    [fix] Moved OrganType to package enums
    To improve code organization

--- src/main/java/com/erguidos/ichor/enums/OrganType.java
+++ src/main/java/com/erguidos/ichor/enums/OrganType.java
@@ -1,4 +1,4 @@
-package com.erguidos.ichor.entity;
+package com.erguidos.ichor.enums;
  
 public enum OrganType {
 	LEFT_LUNG(6), RIGHT_LUNG(6), LEFT_KIDNEY(36), RIGHT_KIDNEY(36), LIVER(12), PANCREAS(15), HEART(4), INTESTINES(8);
