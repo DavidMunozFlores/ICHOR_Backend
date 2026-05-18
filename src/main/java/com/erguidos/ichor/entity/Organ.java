d189ea30e69accc76e609d6e0f673b8d3f2465f8
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-18 22:46:13 +0200

    [feat] Organ Entity + OrganType Created
    Life time added to each organ in TypeOrgan constructor
    
    HLA matches by chains of String in Organ for compatibility

---
+++ src/main/java/com/erguidos/ichor/entity/Organ.java
@@ -0,0 +1,66 @@
+package com.erguidos.ichor.entity;
+ 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+ 
+@Entity
+@Table(name="organs")
+public class Organ {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_organ")
+	private Long id;
+	
+	@Column(name = "organ_type")
+	private OrganType organType;
+	
+	@Column(name="weight_grams")
+	private Double weightGrams;
+	
+	@Column(name="volume_c_c")
+	private Double volumeCC;
+	
+	/**
+	 * Chain must be a six ordered hla markers to match compatibility
+	 */
+	@Column(name="hla_chain")
+	private String hlaChain;
+	
+	@Column(name="")
+	private Hospital donorHospital;
+	
+	@Column(name="assigned")
+	private boolean assigned;
+	
+	protected Organ() {}
+
+	public OrganType getOrganType() {
+		return organType;
+	}
+
+	public Double getWeightGrams() {
+		return weightGrams;
+	}
+
+	public Double getVolumeCC() {
+		return volumeCC;
+	}
+
+	public String getHlaChain() {
+		return hlaChain;
+	}
+
+	public Hospital getDonorHospital() {
+		return donorHospital;
+	}
+
+	public boolean isAssigned() {
+		return assigned;
+	}
+	
+	
+}
