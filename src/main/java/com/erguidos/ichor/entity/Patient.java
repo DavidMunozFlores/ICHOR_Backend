c80140aade01c432a39d219e654f56aa48a12555
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-18 23:01:13 +0200

    Created Patient
    Dni atribute as identification

---
+++ src/main/java/com/erguidos/ichor/entity/Patient.java
@@ -0,0 +1,45 @@
+package com.erguidos.ichor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "patients")
+public class Patient extends BaseEntity  {
+
+	@Column(name = "internal_id")
+	private String internalID;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "identification")
+	private String identification;
+	
+	@Column(name = "blood_type")
+	@Enumerated(EnumType.STRING)
+	private BloodType bloodType;
+	
+	protected Patient() {}
+
+	public String getInternalID() {
+		return internalID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getIdentification() {
+		return identification;
+	}
+
+	public BloodType getBloodType() {
+		return bloodType;
+	}
+	
+	
+}
