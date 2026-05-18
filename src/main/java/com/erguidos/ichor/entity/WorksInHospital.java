6160d5c448967d1a37a3ea125bbb5902f69536f2
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-18 23:50:59 +0200

    [fix] Updated WorksInHospital hospital added
    Each worker's own hospital added

--- src/main/java/com/erguidos/ichor/entity/WorksInHospital.java
+++ src/main/java/com/erguidos/ichor/entity/WorksInHospital.java
@@ -1,10 +1,14 @@
 package com.erguidos.ichor.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class WorksInHospital
                 extends User {
+	
+	@Column(name = "")
+	private Hospital hopsital;
     
     protected WorksInHospital() {}
 }
