db1f95eb0163cd0e7e10096a6f5b48d016341f9a
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-18 11:39:03 +0200

    Spring Initializr Project
    Just created the project
    Maven
    Spring Boot 4.0.6
    Properties
    
    -Spring Web
    -MySql Driver
    -JPA
    -OpenAPI
    -DevTools
    -H2
    -Doc

---
+++ src/test/java/com/erguidos/ichor/IchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erguidos.ichor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
