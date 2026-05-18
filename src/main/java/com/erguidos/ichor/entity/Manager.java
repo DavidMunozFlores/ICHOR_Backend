108e695bac2481c6b928eaaa10a6bc3b78f47145
Guido Giova <guido.giovaDAM@gmail.com>
2026-05-18 13:57:37 +0200

    [feat] Created Manager Entity

---
+++ src/main/java/com/erguidos/ichor/entity/Manager.java
@@ -0,0 +1,10 @@
+package com.erguidos.ichor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "managers")
+public class Manager
+       extends User {
+}
