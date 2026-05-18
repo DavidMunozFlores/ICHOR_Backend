64cea3bcd2f8c6f5f1ce42d242f6bf572fd9c5e1
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-18 23:47:47 +0200

    [feat] Created Organ Petition
    Thinking About: Create a superclass with shared attributes with Organ (type, weight, volume, etc)

---
+++ src/main/java/com/erguidos/ichor/entity/OrganPetition.java
@@ -0,0 +1,80 @@
+package com.erguidos.ichor.entity;
+
+import com.erguidos.ichor.enums.OrganPetitionState;
+import com.erguidos.ichor.enums.OrganType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "organ_petitions")
+public class OrganPetition extends BaseEntity {
+
+	@Column(name = "organ_type")
+	@Enumerated(EnumType.STRING)
+	private OrganType organType;
+	
+	@Column(name="weight_grams")
+	private Double weightGrams;
+	
+	@Column(name="volume_c_c")
+	private Double volumeCC;
+	
+	@Column(name="hla_chain")
+	private String hlaChain;
+	
+	@Column(name="")
+	private Hospital donorHospital;
+	
+	@Column(name = "")
+	private Doctor doctor;
+	
+	@Column(name="")
+	private Patient patient;
+	
+	@Column(name = "organ_petition_state")
+	private OrganPetitionState organPetitionState;
+	
+	@Column(name = "")
+	private Organ organ;
+	
+	protected OrganPetition() {}
+
+	public OrganType getOrganType() {
+		return organType;
+	}
+
+	public Double getWeightGrams() {
+		return weightGrams;
+	}
+
+	public Double getVolumeCC() {
+		return volumeCC;
+	}
+
+	public String getHlaChain() {
+		return hlaChain;
+	}
+
+	public Hospital getDonorHospital() {
+		return donorHospital;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public OrganPetitionState getOrganPetitionState() {
+		return organPetitionState;
+	}
+
+	public Organ getOrgan() {
+		return organ;
+	}
+	
+	
+	
+}
