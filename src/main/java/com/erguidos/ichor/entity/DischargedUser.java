1c7eaea07d69d3ad4321e1c45a3e2ed7dcfe6e35
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-20 13:09:32 +0200

    [fix] Added not null in userDischarged

--- src/main/java/com/erguidos/ichor/entity/DischargedUser.java
+++ src/main/java/com/erguidos/ichor/entity/DischargedUser.java
@@ -1,6 +1,5 @@
 package com.erguidos.ichor.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,22 +11,18 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "discharged_users")
 public class DischargedUser {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
 	
     @OneToOne
-    @JoinColumn(name = "id_user")
+    @JoinColumn(name = "id_user", nullable = false)
 	private User userDischarged;
 	
 	protected DischargedUser() {}
     
 	public Long getId() { return this.id; }
 
-	public User getUserDischarged() {
-		return userDischarged;
-	}
-	
-	
+	public User getUserDischarged() { return userDischarged; }
 }
