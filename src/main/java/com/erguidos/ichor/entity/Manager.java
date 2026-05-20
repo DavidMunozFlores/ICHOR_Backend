2a3edde97b08208c4ae1b4044ca20687cb701362
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-21 01:15:32 +0200

    [feat] Updated Manager
    Added PKJC

--- src/main/java/com/erguidos/ichor/entity/Manager.java
+++ src/main/java/com/erguidos/ichor/entity/Manager.java
@@ -1,10 +1,12 @@
 package com.erguidos.ichor.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "managers")
+@PrimaryKeyJoinColumn(name = "id_user")
 public class Manager extends User {
     
     protected Manager() {}
