8d7d33ee214cff9411b2b72859ca7d58e4b4f9da
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-21 01:18:20 +0200

    [fix] Updated Patient
    Added Hospital
    Unique into identification

--- src/main/java/com/erguidos/ichor/entity/Patient.java
+++ src/main/java/com/erguidos/ichor/entity/Patient.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -28,7 +30,7 @@ public class Patient  {
 	@Column(name = "name", nullable = false, length = 255)
 	private String name;
 	
-	@Column(name = "identification", nullable = false, length = 255)
+	@Column(name = "identification", unique = true, nullable = false, length = 255)
 	private String identification;
 	
 	@Column(name = "blood_type", nullable = false)
@@ -40,6 +42,10 @@ public class Patient  {
 	
 	@Column(name = "weight", nullable = false)
 	private Double weight;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_hospital")
+	private Hospital hospital;
 	
 	@OneToMany(mappedBy = "patient")
 	private List<OrganPetition> organPetitions;
@@ -55,4 +61,12 @@ public class Patient  {
 	public String getIdentification() { return identification; }
 
 	public BloodType getBloodType() { return bloodType; }
+
+	public Double getHeight() { return height; }
+
+	public Double getWeight() { return weight; }
+
+	public Hospital getHospital() { return hospital; }
+
+	public List<OrganPetition> getOrganPetitions() { return organPetitions; }
 }
