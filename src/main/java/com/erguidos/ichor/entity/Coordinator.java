8ebdc0886819afa782377d74f3ebc80cd98f16d1
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-21 01:13:02 +0200

    [fix] Updated Coordinator
    Extends User
    Has hospital
    Has registered organs

--- src/main/java/com/erguidos/ichor/entity/Coordinator.java
+++ src/main/java/com/erguidos/ichor/entity/Coordinator.java
@@ -1,11 +1,29 @@
 package com.erguidos.ichor.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "coordinators")
-public class Coordinator extends WorksInHospital {
+@PrimaryKeyJoinColumn(name = "id_user")
+public class Coordinator extends User {
+	
+	@ManyToOne
+	@JoinColumn(name="id_hospital", nullable = false)
+	private Hospital hospital;
+	
+	@OneToMany(mappedBy = "coordinator")
+	private List<Organ> registeredOrgans;
     
     protected Coordinator() {}
+
+	public Hospital getHospital() { return hospital; }
+
+	public List<Organ> getRegisteredOrgans() { return registeredOrgans; }
 }
