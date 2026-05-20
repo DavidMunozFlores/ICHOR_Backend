603798eeed8f6e63b019b1f38ccbfc461575e22e
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-20 13:22:35 +0200

    [fix] Updated WorksInHospital
    Now is an Entity and has non nullable fields

--- src/main/java/com/erguidos/ichor/entity/WorksInHospital.java
+++ src/main/java/com/erguidos/ichor/entity/WorksInHospital.java
@@ -1,22 +1,17 @@
 package com.erguidos.ichor.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MappedSuperclass;
 
-@MappedSuperclass
-public abstract class WorksInHospital
-                extends User {
+@Entity
+public abstract class WorksInHospital extends User {
 	
 	@ManyToOne
-	@JoinColumn(name="id_hospital")
+	@JoinColumn(name="id_hospital", nullable = false)
 	private Hospital hopsital;
     
     protected WorksInHospital() {}
 
-	public Hospital getHopsital() {
-		return hopsital;
-	}
-    
-    
+	public Hospital getHopsital() { return hopsital; }
 }
