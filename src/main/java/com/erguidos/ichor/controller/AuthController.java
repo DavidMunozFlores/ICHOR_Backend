4ba2624fffb90960ebe005aacf8bc116cf18f6ed
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-22 13:26:20 +0200

    [feat] Created AuthController
    Log-in method

---
+++ src/main/java/com/erguidos/ichor/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.erguidos.ichor.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erguidos.ichor.dto.request.UserRequestDTO;
+import com.erguidos.ichor.dto.response.IsAuthorizedDTO;
+import com.erguidos.ichor.service.AuthService;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+	private AuthService authService;
+	
+	AuthController(AuthService authService) {
+		this.authService = authService;
+	}
+	
+	@PostMapping("/log-in")
+	public ResponseEntity<IsAuthorizedDTO> loginUser(@RequestBody UserRequestDTO userRequestDTO) {
+		return ResponseEntity.ok(authService.loginUser(userRequestDTO));
+	}
+}
