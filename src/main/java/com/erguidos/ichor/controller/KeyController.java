416d0892ae3b388e4345209ac09df137d66753f4
Guido Giova <guido.giovaDAM@gmail.com>
2026-05-23 14:08:01 +0200

    [feat] Adde decrypt endpoint for testing

--- src/main/java/com/erguidos/ichor/controller/KeyController.java
+++ src/main/java/com/erguidos/ichor/controller/KeyController.java
@@ -1,10 +1,12 @@
 package com.erguidos.ichor.controller;
 
+import java.security.GeneralSecurityException;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,14 @@ public class KeyController {
                                 .cachePublic()
                 )
                 .body(new PublicKeyResponse(keyService.getPublicKey()));
+    }
+    
+    @PostMapping("/decrypt")
+    public ResponseEntity<?> decrypt(@RequestBody DecryptRequest request) {
+        try {
+            return ResponseEntity.ok(this.keyService.decrypt(request.data()));
+        } catch (GeneralSecurityException e) {
+            return ResponseEntity.ok(e.getMessage());
+        }
     }
 }
