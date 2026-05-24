3204a36a449814596c2bbbd11735f097e2d56b52
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-25 00:05:45 +0200

    [feat] Added Cors to KeyController

--- src/main/java/com/erguidos/ichor/controller/KeyController.java
+++ src/main/java/com/erguidos/ichor/controller/KeyController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.erguidos.ichor.service.KeyServiceInterface;
 
 @RestController
 @RequestMapping("/api/v1/keys")
+@CrossOrigin(origins = "http://localhost:4200")
 public class KeyController {
     private final KeyServiceInterface keyService;
     
