c60016ca9382ab5a51b15725ba3f824ed222bed1
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-26 11:58:44 +0200

    [fix] Updated DoctorService
    Now can create doctors hashing password and transforming decryptData to AuthenticatedRequest

--- src/main/java/com/erguidos/ichor/service/doctor/DoctorService.java
+++ src/main/java/com/erguidos/ichor/service/doctor/DoctorService.java
@@ -2,24 +2,21 @@ package com.erguidos.ichor.service.doctor;
 
 import java.security.GeneralSecurityException;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.erguidos.ichor.component.HashInterface;
 import com.erguidos.ichor.dto.request.AuthenticatedRequest;
-import com.erguidos.ichor.dto.request.CreateUserRequestDTO;
-import com.erguidos.ichor.dto.request.CreateUserRequestDTOV2;
-import com.erguidos.ichor.dto.request.CreateUserRequest;
 import com.erguidos.ichor.dto.request.DecryptRequest;
+import com.erguidos.ichor.dto.response.WorkerCreatedResponse;
 import com.erguidos.ichor.entity.Doctor;
 import com.erguidos.ichor.entity.Hospital;
 import com.erguidos.ichor.entity.Manager;
-import com.erguidos.ichor.entity.User;
 import com.erguidos.ichor.exceptions.IncorrectPasswordException;
-import com.erguidos.ichor.exceptions.UserAlreadyExistsException;
 import com.erguidos.ichor.exceptions.UserNotFoundException;
 import com.erguidos.ichor.repository.DoctorRepository;
 import com.erguidos.ichor.repository.HospitalRepository;
 import com.erguidos.ichor.repository.ManagerRepository;
-import com.erguidos.ichor.repository.UserRepository;
 import com.erguidos.ichor.service.key.KeyService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -31,70 +28,55 @@ public class DoctorService implements DoctorServiceInterface {
 	private static final String DOCTOR_EXISTS_MSJ = "The Doctor already exists";
 	private static final String HOSPITAL_NOT_EXISTS_MSJ = "The Hospital doesn't exist";
 	private static final String USER_NOT_EXISTS_MSJ = "The User doesn't exist";
-	private static final String ROL_NOT_CORRECT_MSJ = "That rol cannot create users";
 	private static final String PASSWORD_INCORRECT_MSJ = "The password isn't correct";
-	
+	private static final String DOCTOR_CREATED_MSJ = "DOCTOR CREATED";
+
 	private DoctorRepository doctorRepository;
 	private HospitalRepository hospitalRepository;
 	private ManagerRepository managerRepository;
 	private KeyService keyService;
-	
-	DoctorService(DoctorRepository doctorRepository, HospitalRepository hospitalRepository, ManagerRepository managerRepository, KeyService keyService) {
+	private HashInterface hashComponent;
+
+	DoctorService(
+			DoctorRepository doctorRepository,
+			HospitalRepository hospitalRepository,
+			ManagerRepository managerRepository,
+			KeyService keyService,
+			@Qualifier("bcryptPasswordEncoder") HashInterface hashComponent
+			) {
+		
 		this.doctorRepository = doctorRepository;
 		this.hospitalRepository = hospitalRepository;
 		this.managerRepository = managerRepository;
 		this.keyService = keyService;
+		this.hashComponent = hashComponent;
 	}
 
 	@Override
-	public void createDoctor(DecryptRequest decReq) {
-		
-		AuthenticatedRequest<CreateUserRequest> authReq;
-		
-		try {
-			authReq = keyService.decryptToObject(decReq, AuthenticatedRequest.class);
-			
-			if(doctorRepository.existsByUsername(authReq.data().username()))
-				throw new EntityExistsException(DOCTOR_EXISTS_MSJ);
-			
-			Hospital doctorHospital = hospitalRepository
-					.findById(authReq.data().idHospital())
-					.orElseThrow(() -> new EntityNotFoundException(HOSPITAL_NOT_EXISTS_MSJ));
-			
-			doctorRepository.save(
-					Doctor.create(authReq.data().username(), authReq.data().password(), doctorHospital)
-					);
-		} catch (JsonProcessingException | GeneralSecurityException e) {
-			
-		}
-		
+	public WorkerCreatedResponse createDoctor(DecryptRequest decReq)
+			throws JsonProcessingException, GeneralSecurityException {
 
-	}
-	
-	@Override
-	public void createDoctorV2(CreateUserRequestDTOV2 createUserRequestDTO) {
-		
-		isValidManager(createUserRequestDTO);
-		
-		if(doctorRepository.existsByUsername(createUserRequestDTO.username()))
-			throw new UserAlreadyExistsException(DOCTOR_EXISTS_MSJ);
-		
-		Hospital doctorHospital = hospitalRepository
-				.findById(createUserRequestDTO.idHospital())
-				.orElseThrow(() -> new EntityNotFoundException(HOSPITAL_NOT_EXISTS_MSJ));
-		
-		// TODO bycrypt
-		doctorRepository.save(
-				Doctor.create(createUserRequestDTO.username(), createUserRequestDTO.password(), doctorHospital)
-				);
-	}
+		AuthenticatedRequest autReq =
+				keyService.decryptToObject(decReq, AuthenticatedRequest.class);
 
-	private void isValidManager(CreateUserRequestDTOV2 createUserRequestDTO) {
-		Manager manager = managerRepository
-				.findManagerByUsername(createUserRequestDTO.managerData().username())
+		Manager manager = managerRepository.findManagerByUsername(autReq.authCredentials().username())
 				.orElseThrow(() -> new UserNotFoundException(USER_NOT_EXISTS_MSJ));
-		
-		if(!manager.getPassword().equals(createUserRequestDTO.managerData().password()))
+
+		if (!manager.getPassword().equals(autReq.authCredentials().password()))
 			throw new IncorrectPasswordException(PASSWORD_INCORRECT_MSJ);
+
+		if (doctorRepository.existsByUsername(autReq.data().username()))
+			throw new EntityExistsException(DOCTOR_EXISTS_MSJ);
+
+		Hospital doctorHospital = hospitalRepository.findById(autReq.data().idHospital())
+				.orElseThrow(() -> new EntityNotFoundException(HOSPITAL_NOT_EXISTS_MSJ));
+
+		doctorRepository.save(Doctor.create(
+				autReq.data().username(),
+				hashComponent.hashPassword(autReq.data().password()),
+				doctorHospital));
+		
+		
+		return new WorkerCreatedResponse(DOCTOR_CREATED_MSJ);
 	}
 }
