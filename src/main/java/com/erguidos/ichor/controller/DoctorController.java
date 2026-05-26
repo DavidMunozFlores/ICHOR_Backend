947edd864a2ba75116384bea24c99250370558d4
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-26 11:56:23 +0200

    [fix] Now DoctorController receives Decrypt

--- src/main/java/com/erguidos/ichor/controller/DoctorController.java
+++ src/main/java/com/erguidos/ichor/controller/DoctorController.java
@@ -1,5 +1,6 @@
 package com.erguidos.ichor.controller;
 
+import java.security.GeneralSecurityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,11 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.erguidos.ichor.dto.request.CreateUserRequestDTO;
-import com.erguidos.ichor.dto.request.CreateUserRequestDTOV2;
 import com.erguidos.ichor.dto.request.DecryptRequest;
+import com.erguidos.ichor.dto.response.WorkerCreatedResponse;
 import com.erguidos.ichor.service.doctor.DoctorServiceInterface;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 
 @RestController
@@ -26,17 +26,9 @@ public class DoctorController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<String> createDoctor(@RequestBody DecryptRequest decryptDTO){
+	public ResponseEntity<WorkerCreatedResponse> createDoctor(@RequestBody DecryptRequest decryptDTO)
+			throws JsonProcessingException, GeneralSecurityException {
 		
-		doctorService.createDoctor(decryptDTO);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body("DOCTOR CREATED");
-	}
-	
-	@PostMapping("/create2")
-	public ResponseEntity<String> createDoctor(@RequestBody CreateUserRequestDTOV2 doctorRequestDTO){
-		doctorService.createDoctorV2(doctorRequestDTO);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body("DOCTOR CREATED");
+		return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.createDoctor(decryptDTO));
 	}
 }
