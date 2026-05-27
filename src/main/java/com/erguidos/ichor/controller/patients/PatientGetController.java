d0a303f1ef0c96f43b28722fd7ff6b0483cf5b05
Guido Giova <guido.giovaDAM@gmail.com>
2026-05-27 11:33:26 +0200

    [feat] Created PatientGetController

---
+++ src/main/java/com/erguidos/ichor/controller/patients/PatientGetController.java
@@ -0,0 +1,18 @@
+package com.erguidos.ichor.controller.patients;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.erguidos.ichor.service.patient.PatientServiceInterface;
+
+@RestController
+@RequestMapping("/api/v1/coordinators")
+@CrossOrigin(origins = {"${app.cors.allowed}"})
+public class PatientGetController {
+    private final PatientServiceInterface patientService;
+    
+    PatientGetController(PatientServiceInterface patientService) {
+        this.patientService = patientService;
+    }
+}
