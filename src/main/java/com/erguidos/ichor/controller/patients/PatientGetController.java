7318c603fabd24601869bb7c4cba3575eded1dae
Guido Giova <guido.giovaDAM@gmail.com>
2026-05-27 12:35:09 +0200

    [feat] Added getAllPatients endpoint

--- src/main/java/com/erguidos/ichor/controller/patients/PatientGetController.java
+++ src/main/java/com/erguidos/ichor/controller/patients/PatientGetController.java
@@ -1,10 +1,16 @@
 package com.erguidos.ichor.controller.patients;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erguidos.ichor.dto.response.PatientResponse;
 import com.erguidos.ichor.service.patient.PatientServiceInterface;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/api/v1/patients")
@@ -16,5 +22,9 @@ public class PatientGetController {
         this.patientService = patientService;
     }
     
+    @GetMapping
+    public ResponseEntity<List<PatientResponse>> getAllPatients() {
+        return ResponseEntity.ok(this.patientService.getAllPatients());
+    }
     
 }
