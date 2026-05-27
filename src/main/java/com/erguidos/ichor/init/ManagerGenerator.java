b39ca514a3444d324a67127d6142ca3cb7de90c0
Guido Giova <guido.giovaDAM@gmail.com>
2026-05-27 08:32:25 +0200

    [feat] ManagerGenerator hashes password

--- src/main/java/com/erguidos/ichor/init/ManagerGenerator.java
+++ src/main/java/com/erguidos/ichor/init/ManagerGenerator.java
@@ -1,8 +1,11 @@
 package com.erguidos.ichor.init;
 
 import com.erguidos.ichor.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import com.erguidos.ichor.component.HashInterface;
 import com.erguidos.ichor.entity.Manager;
 import com.erguidos.ichor.repository.ManagerRepository;
 
@@ -10,17 +13,23 @@ import com.erguidos.ichor.repository.ManagerRepository;
 public class ManagerGenerator {
     private final ManagerRepository managerRepository;
     private final UserRepository userRepository;
+    private final HashInterface hashing;
     
-    ManagerGenerator(ManagerRepository managerRepository, UserRepository userRepository) {
+    ManagerGenerator(
+        ManagerRepository managerRepository,
+        UserRepository userRepository,
+        @Qualifier("bcryptPasswordEncoder") HashInterface hashing
+    ) {
         this.managerRepository = managerRepository;
         this.userRepository = userRepository;
+        this.hashing = hashing;
     }
     
     public void generate() {
         if (this.userRepository.findUserByUsername("managerCreator").isEmpty()) {
             Manager manager = Manager.builder()
                     .setUsername("managerCreator")
-                    .setPassword("1234")
+                    .setPassword(this.hashing.hashPassword("1234"))
                     .build();
             
             this.managerRepository.save(manager);
