b7f514b9617e701afc624dea6552e781466eb24f
Guido Giova <guido.giovaDAM@gmail.com>
2026-05-27 12:17:18 +0200

    [fix] Added Repository annotation

--- src/main/java/com/erguidos/ichor/repository/CoordinatorRepository.java
+++ src/main/java/com/erguidos/ichor/repository/CoordinatorRepository.java
@@ -1,9 +1,11 @@
 package com.erguidos.ichor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.erguidos.ichor.entity.Coordinator;
 
+@Repository
 public interface CoordinatorRepository
          extends JpaRepository<Coordinator, Long> {
 }
