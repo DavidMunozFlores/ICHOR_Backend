df5d83481c7397b5ea757fc86f29ab0655a5596f
Adrian <114065470+adriande01@users.noreply.github.com>
2026-05-30 20:02:31 +0200

    [feat] Update CoordinatorRepository
    findByUsername used in Organ Service

--- src/main/java/com/erguidos/ichor/repository/CoordinatorRepository.java
+++ src/main/java/com/erguidos/ichor/repository/CoordinatorRepository.java
@@ -1,9 +1,13 @@
 package com.erguidos.ichor.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.erguidos.ichor.entity.Coordinator;
 
 public interface CoordinatorRepository
          extends JpaRepository<Coordinator, Long> {
+	
+	Optional<Coordinator> findByUsername(String username);
 }
