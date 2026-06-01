1ef774a833c0618d6fc34de177babd4e6273fb66
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-01 07:59:48 +0200

    [fix] Change annotation to @PathVariable

--- src/main/java/com/erguidos/ichor/controller/coordinators/CoordinatorGetController.java
+++ src/main/java/com/erguidos/ichor/controller/coordinators/CoordinatorGetController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,7 +23,7 @@ public class CoordinatorGetController {
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<?> getCoordinator(@RequestParam Long id) {
+    public ResponseEntity<?> getCoordinator(@PathVariable Long id) {
         return this.semi.getCoordinator(id);
     }
     
