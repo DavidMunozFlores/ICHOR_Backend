f5efcc133ae3b148a16b662cedf13ac4cf57c981
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-02 11:18:34 +0200

    [fix] Added bloodtype into RegisterOrganRequest

--- src/main/java/com/erguidos/ichor/dto/request/RegisterOrganRequest.java
+++ src/main/java/com/erguidos/ichor/dto/request/RegisterOrganRequest.java
@@ -1,11 +1,13 @@
 package com.erguidos.ichor.dto.request;
 
+import com.erguidos.ichor.enums.BloodType;
 import com.erguidos.ichor.enums.OrganType;
 
 public record RegisterOrganRequest(
 		OrganType organType,
 		Double weightGrams,
 		Double volumeCC,
-		String hla
+		String hla,
+		BloodType bloodType
 		) implements DataRequestInterface {}
 
