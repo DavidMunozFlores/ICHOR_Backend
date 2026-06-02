696aaa39760ec96795490273eacef58c0acabec6
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-02 11:28:16 +0200

    [feat] Updated OrganServiceInterface
    getOrganBloodTypeInfo

--- src/main/java/com/erguidos/ichor/service/organ/OrganServiceInterface.java
+++ src/main/java/com/erguidos/ichor/service/organ/OrganServiceInterface.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.erguidos.ichor.dto.request.AuthenticatedRequest;
 import com.erguidos.ichor.dto.request.RegisterOrganRequest;
+import com.erguidos.ichor.dto.response.OrganBloodTypeInfoResponse;
 import com.erguidos.ichor.dto.response.OrganTypeInfoResponse;
 import com.erguidos.ichor.dto.response.RegisterOrganResponse;
 
 public interface OrganServiceInterface {
 	List<OrganTypeInfoResponse> getOrganTypeInfo();
+	List<OrganBloodTypeInfoResponse> getOrganBloodTypeInfo();
 	RegisterOrganResponse registerOrgan(AuthenticatedRequest<RegisterOrganRequest> ar);
 }
