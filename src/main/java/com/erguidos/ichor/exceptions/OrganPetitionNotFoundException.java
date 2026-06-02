a5ac2966771a226a2d9f8d338ab4d66d56a39177
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-02 11:13:26 +0200

    [feat] Added OtganPet Exceptions

---
+++ src/main/java/com/erguidos/ichor/exceptions/OrganPetitionNotFoundException.java
@@ -0,0 +1,10 @@
+package com.erguidos.ichor.exceptions;
+
+public class OrganPetitionNotFoundException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = 1L;
+
+	public OrganPetitionNotFoundException(String msj) {
+		super(msj);
+	}
+}
