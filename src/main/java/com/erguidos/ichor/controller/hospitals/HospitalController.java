a3677219b730b4a1fe3a81e206eaa9981436d16a
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-02 10:05:00 +0200

    [feat] Made HospitalController handle the request

--- src/main/java/com/erguidos/ichor/controller/hospitals/HospitalController.java
+++ src/main/java/com/erguidos/ichor/controller/hospitals/HospitalController.java
@@ -3,6 +3,14 @@ package com.erguidos.ichor.controller.hospitals;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erguidos.ichor.dto.mappers.HospitalMapper;
+import com.erguidos.ichor.dto.mappers.ListMapper;
+import com.erguidos.ichor.dto.response.HospitalResponse;
+import com.erguidos.ichor.dto.response.ListWrapper;
+import com.erguidos.ichor.dto.types.SearchType;
+import com.erguidos.ichor.entity.Hospital;
+import com.erguidos.ichor.service.hospital.HospitalServiceInterface;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +19,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api/v1/hospitals")
 @CrossOrigin(origins = {"${app.cors.allowed}"})
 public class HospitalController {
-    private final HospitalSemiController semi;
+    private final HospitalServiceInterface hospitalService;
     
-    HospitalController(HospitalSemiController semi) {
-        this.semi = semi;
+    HospitalController(HospitalServiceInterface hospitalService) {
+        this.hospitalService = hospitalService;
     }
     
     @GetMapping
-    public ResponseEntity<?> getAllHospitals() {
-        return this.semi.getAllHospitals();
+    public ResponseEntity<ListWrapper<HospitalResponse>> getAllHospitals() {
+        return ResponseEntity.ok(
+            ListMapper.toListWrapper(
+                this.hospitalService.getAllHospitals(),
+                HospitalMapper::toHospitalResponse
+            )
+        );
+    }
+    
+    public ResponseEntity<HospitalResponse> getHospital(Long id) {
+        SearchType<Hospital> response = this.hospitalService.getHospital(id);
+        return switch (response) {
+            case SearchType.Found(Hospital hospital) ->
+                ResponseEntity.ok(HospitalMapper.toHospitalResponse(hospital));
+            case SearchType.Failed() ->
+                ResponseEntity.notFound().build();
+        };
     }
 }
