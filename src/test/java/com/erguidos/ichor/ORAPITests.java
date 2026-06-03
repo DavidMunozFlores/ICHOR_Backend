bb20e44aa5663a738569ecfced87c2c05c33e59d
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-03 10:25:34 +0200

    [feat] Added API Services

--- src/test/java/com/erguidos/ichor/ORAPITests.java
+++ src/test/java/com/erguidos/ichor/ORAPITests.java
@@ -9,20 +9,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.erguidos.ichor.service.open_route_api.ORAPIService;
 import com.erguidos.ichor.service.open_route_api.ORAPIServiceInterface;
 
+@SpringBootTest
 class ORAPITests {
 
-	
-	@Test
-	void getDuration() {
-//		ORAPIServiceInterface s = new ORAPIService();
-//		double d = s.getDurationBetweenTwoHospitalPoints(
-//														new BigDecimal("8.681495"),
-//														new BigDecimal("49.41461"),
-//														new BigDecimal("8.687872"),
-//														new BigDecimal("49.420318"));
-//		//double d = s.getDurationBetweenTwoHospitalPoints(-3.68913000, 40.47743000, 2.15250000, 41.38790000);
-//		System.out.println(d);
-	}
+    @Autowired
+    private ORAPIServiceInterface service;
+
+    @Test
+    void getDuration() {
+
+        double d = service.getDurationBetweenTwoHospitalPoints(
+                new BigDecimal("8.681495"),
+                new BigDecimal("49.41461"),
+                new BigDecimal("8.687872"),
+                new BigDecimal("49.420318")
+        );
+
+        System.out.println(d);
+    }
 }
 
 
