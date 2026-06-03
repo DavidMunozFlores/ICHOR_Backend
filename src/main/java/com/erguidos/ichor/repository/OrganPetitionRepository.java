b9c5a5ffbb261031287a78ca3a8b30447d015cf8
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-03 09:49:43 +0200

    [feat] Added Query methods in OrganPetitionRepository
    To check compatibility, I prefer to use DBB instead of full streams

--- src/main/java/com/erguidos/ichor/repository/OrganPetitionRepository.java
+++ src/main/java/com/erguidos/ichor/repository/OrganPetitionRepository.java
@@ -1,9 +1,23 @@
 package com.erguidos.ichor.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.erguidos.ichor.entity.OrganPetition;
+import com.erguidos.ichor.enums.BloodType;
+import com.erguidos.ichor.enums.OrganPetitionState;
+import com.erguidos.ichor.enums.OrganType;
 
 @Repository
-public interface OrganPetitionRepository extends JpaRepository<OrganPetition, Long> {}
+public interface OrganPetitionRepository extends JpaRepository<OrganPetition, Long> {
+	Set<OrganPetition> findByOrganPetitionState(OrganPetitionState organPetitionState);
+	Set<OrganPetition> findByOrganType(OrganType organType);
+	
+	@Query("SELECT op FROM OrganPetition op JOIN op.patient p where p.bloodType IN :bloodTypes")
+	Set<OrganPetition> findByCompatibleBlood(@Param(value = "bloodTypes") List<BloodType> bloodTypes);
+}
