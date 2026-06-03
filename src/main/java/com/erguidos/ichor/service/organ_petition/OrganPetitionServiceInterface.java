bec095ede093d2366b74e9784f48a6f060c3ea36
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-03 10:00:23 +0200

    [feat] Added method in OrganPetitionServiceInterface
    findByOrganPetitionState

--- src/main/java/com/erguidos/ichor/service/organ_petition/OrganPetitionServiceInterface.java
+++ src/main/java/com/erguidos/ichor/service/organ_petition/OrganPetitionServiceInterface.java
@@ -1,12 +1,17 @@
 package com.erguidos.ichor.service.organ_petition;
 
 import com.erguidos.ichor.dto.request.StateUpdateOrganPetitionRequest;
+
+import java.util.Set;
+
 import com.erguidos.ichor.dto.request.AuthenticatedRequest;
 import com.erguidos.ichor.dto.request.OrganPetitionRequest;
 import com.erguidos.ichor.dto.response.OrganPetitionResponse;
+import com.erguidos.ichor.enums.OrganPetitionState;
 
 public interface OrganPetitionServiceInterface {
 	OrganPetitionResponse createOrganPetition(AuthenticatedRequest<OrganPetitionRequest> ar);
 	OrganPetitionResponse acceptOrganPetition(AuthenticatedRequest<StateUpdateOrganPetitionRequest> ar);
 	OrganPetitionResponse cancellOrganPetition(AuthenticatedRequest<StateUpdateOrganPetitionRequest> ar);
+	Set<OrganPetitionResponse> findByOrganPetitionState(OrganPetitionState organPetitionState);
 }
