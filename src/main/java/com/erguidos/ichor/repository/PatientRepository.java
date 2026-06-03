fb71ea36ded09d01427683cd7dd7437e6f20b14a
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-03 09:52:14 +0200

    [feat] Added method in PatientRepository
    findByBloodTypeIn, I used Set cause contains instead of List

--- src/main/java/com/erguidos/ichor/repository/PatientRepository.java
+++ src/main/java/com/erguidos/ichor/repository/PatientRepository.java
@@ -1,11 +1,14 @@
 package com.erguidos.ichor.repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.erguidos.ichor.entity.Patient;
+import com.erguidos.ichor.enums.BloodType;
 
 
 @Repository
@@ -14,4 +17,6 @@ public interface PatientRepository
     Optional<Patient> getByInternalID(String internalID);
 
     Optional<Patient> getByIdentification(String identification);
+    
+    Set<Patient> findByBloodTypeIn(List<BloodType> bloodTypes);
 }
