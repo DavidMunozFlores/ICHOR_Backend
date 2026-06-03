957292f8ba671a66409bfbf0f3d84c2db52dd5e5
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-03 23:18:13 +0200

    [fix] Change name to match frontend

--- src/main/java/com/erguidos/ichor/dto/response/WorkerResponse.java
+++ src/main/java/com/erguidos/ichor/dto/response/WorkerResponse.java
@@ -3,7 +3,7 @@ package com.erguidos.ichor.dto.response;
 import com.erguidos.ichor.enums.Role;
 
 public record WorkerResponse(
-    String name,
+    String username,
     String hospital,
-    Role role
+    Role   role
 ) {}
