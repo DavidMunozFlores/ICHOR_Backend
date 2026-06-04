352870e67a43b38d0712a0f44d42ca4460803238
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-04 12:23:37 +0200

    [fix] Updated changed state cacelled

--- src/main/java/com/erguidos/ichor/entity/OrganPetition.java
+++ src/main/java/com/erguidos/ichor/entity/OrganPetition.java
@@ -34,7 +34,7 @@ public class OrganPetition {
 	private final static String PATIENT_NULL = "Patient cannot be null";
 	private final static String ORGAN_NULL = "Compatible organ cannot be null";
 	private final static String UNOUTHARIZED_STATE_ACCEPT = "Cannot change to WAITING if is not DRAFT";
-	private final static String UNOUTHARIZED_STATE_CANCELL = "Cannot change to WAITING if is not DRAFT";
+	private final static String UNOUTHARIZED_STATE_CANCELL = "Cannot change to CANCELL if is not WAITING or ASSIGNED";
 	
 	private final static Double MAX_DIMENSION = 4000.0;
 	private final static Double MIN_DIMENSION = 10.0;
@@ -163,7 +163,7 @@ public class OrganPetition {
 	}
 	
 	public void cancell() {
-		if(this.organPetitionState != OrganPetitionState.ASSIGNED_ORGAN)
+		if(this.organPetitionState != OrganPetitionState.WAITING && this.organPetitionState != OrganPetitionState.ASSIGNED_ORGAN )
 			throw new UnouthorizedOrganPetitionStateException(UNOUTHARIZED_STATE_CANCELL);
 		
 		this.organPetitionState = OrganPetitionState.CANCELLED;
