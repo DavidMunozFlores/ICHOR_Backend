678882c001eeb1c1ff7bdf9cdb862be305336089
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-04 17:06:11 +0200

    [style] Added spaces

--- src/main/java/com/erguidos/ichor/dto/response/PatientResponse.java
+++ src/main/java/com/erguidos/ichor/dto/response/PatientResponse.java
@@ -5,13 +5,13 @@ import java.util.List;
 import com.erguidos.ichor.entity.OrganPetition;
 
 public record PatientResponse(
-	Long idPatient,
-    String internalID,
-    String name,
-    String identification,
-    String bloodType,
-    Double height,
-    Double weight,
-    Long   idHospital,
+	Long                idPatient,
+    String              internalID,
+    String              name,
+    String              identification,
+    String              bloodType,
+    Double              height,
+    Double              weight,
+    Long                idHospital,
     List<OrganPetition> organPetitions
 ) {}
