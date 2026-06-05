9cbeeb0f8c818eb5d9c5ef5a4893f4ef4f62659b
Adrian <114065470+adriande01@users.noreply.github.com>
2026-06-05 04:22:09 +0200

    [feat] Added complex logic query in OrganPetitionRepository
    Better work in DBB instead of work with streams

--- src/main/java/com/erguidos/ichor/repository/OrganPetitionRepository.java
+++ src/main/java/com/erguidos/ichor/repository/OrganPetitionRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.erguidos.ichor.entity.Hospital;
 import com.erguidos.ichor.entity.OrganPetition;
 import com.erguidos.ichor.enums.BloodType;
 import com.erguidos.ichor.enums.OrganPetitionState;
@@ -15,11 +16,37 @@ import com.erguidos.ichor.enums.OrganType;
 
 @Repository
 public interface OrganPetitionRepository extends JpaRepository<OrganPetition, Long> {
+	@Query("SELECT o FROM OrganPetition o WHERE o.organPetitionState = :organPetitionState")
 	Set<OrganPetition> findByOrganPetitionState(OrganPetitionState organPetitionState);
+	
 	Set<OrganPetition> findByOrganType(OrganType organType);
 	
-	@Query("SELECT op FROM OrganPetition op JOIN op.patient p where p.bloodType IN :bloodTypes")
+	@Query("SELECT op FROM OrganPetition op JOIN op.patient p WHERE p.bloodType IN :bloodTypes")
 	Set<OrganPetition> findByCompatibleBlood(@Param(value = "bloodTypes") List<BloodType> bloodTypes);
 	
-	// Busqueda por muestra
+	@Query("SELECT op FROM OrganPetition op JOIN op.patient p WHERE p.hospital IN :hospitals")
+	Set<OrganPetition> findByCompatibleHospitals(@Param(value = "hospitals") List<Hospital> hospitals);
+	
+	@Query("SELECT op FROM OrganPetition op"
+			+ " JOIN op.patient p"
+			+ " WHERE op.organPetitionState = :organPetitionState"
+			+ " AND op.organType = :organType"
+			+ "	AND p.bloodType IN :bloodTypes"
+			+ "	AND p.hospital IN :hospitals"
+			+ "	AND op.weightGrams"
+			+ " BETWEEN"
+			+ " 	:weightGrams - :range AND :weightGrams + :range"
+			+ " AND op.volumeCC"
+			+ " BETWEEN"
+			+ "		:volumeCC - :range AND :volumeCC + :range"
+			+ "	AND op.organ IS NULL")
+	Set<OrganPetition> findCompatibleOPs(
+						OrganPetitionState organPetitionState,
+						OrganType organType,
+						@Param(value = "bloodTypes") List<BloodType> bloodTypes,
+						@Param(value = "hospitals") List<Hospital> hospitals,
+						@Param(value = "weightGrams") Double weightGrams,
+						@Param(value = "volumeCC") Double volumeCC,
+						@Param(value = "range") Double range
+						);
 }
