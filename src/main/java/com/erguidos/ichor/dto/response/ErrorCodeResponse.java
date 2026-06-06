3afa74fd734f63440ad0818b8fff3aaa547d3f71
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-06 22:49:24 +0200

    [fix] Add JsonIgnore annotation
    Add @JsonIgnore to getResponse to avoid Matryoshka-esque behavior

--- src/main/java/com/erguidos/ichor/dto/response/ErrorCodeResponse.java
+++ src/main/java/com/erguidos/ichor/dto/response/ErrorCodeResponse.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.ResponseEntity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public record ErrorCodeResponse(int status, String message, LocalDateTime time) {
     public static ErrorCodeResponse of(int status, String message) {
         return new ErrorCodeResponse(status, message, LocalDateTime.now());
     }
     
+    @JsonIgnore
     public ResponseEntity<ErrorCodeResponse> getResponse() {
         return ResponseEntity.status(this.status).body(this);
     }
