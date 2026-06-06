4e02241f32925f7faabb1e673244ab3cbe6fb263
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-07 00:05:03 +0200

    [feat] Password validation

--- src/main/java/com/erguidos/ichor/utils/RegexMatching.java
+++ src/main/java/com/erguidos/ichor/utils/RegexMatching.java
@@ -8,13 +8,20 @@ public final class RegexMatching {
     }
     
     private static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{10, 50}$");
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,127}$");
+    
+
+    private static boolean match(Pattern pattern, String toVerify) {
+        return pattern.matcher(toVerify).matches();
+    }
     
     public static boolean isValidUsername(String username) {
         if (username == null) { return false; }
         return RegexMatching.match(USERNAME_PATTERN, username);
     }
     
-    private static boolean match(Pattern pattern, String toVerify) {
-        return pattern.matcher(toVerify).matches();
+    public static boolean isValidPassword(String password) {
+        if (password == null) { return false; }
+        return RegexMatching.match(PASSWORD_PATTERN, password);
     }
 }
