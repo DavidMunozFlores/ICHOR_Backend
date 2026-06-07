7f847e58be24f9a091a6e8c5912cbef64849d787
Guido Giova <guido.giovaDAM@gmail.com>
2026-06-07 22:37:24 +0200

    [fix] Encryption in OrganRegisterController

--- src/main/java/com/erguidos/ichor/controller/organs/OrganRegisterController.java
+++ src/main/java/com/erguidos/ichor/controller/organs/OrganRegisterController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.erguidos.ichor.annotations.AuthenticatedRequestPayload;
 import com.erguidos.ichor.dto.request.AuthenticatedRequest;
 import com.erguidos.ichor.dto.request.RegisterOrganRequest;
 import com.erguidos.ichor.dto.response.RegisterOrganResponse;
 import com.erguidos.ichor.enums.Role;
-import com.erguidos.ichor.service.auth.AuthServiceInterface;
 import com.erguidos.ichor.service.organ.OrganServiceInterface;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,24 +21,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class OrganRegisterController {
 
 	private final OrganServiceInterface organService;
-	private final AuthServiceInterface authService;
 	
-	OrganRegisterController(
-			OrganServiceInterface organService,
-			AuthServiceInterface authService
-			) {
-		
+	OrganRegisterController(OrganServiceInterface organService) {
 		this.organService = organService;
-		this.authService = authService;
 	}
 	
 	@PostMapping("/register-organ")
 	public ResponseEntity<RegisterOrganResponse> registerOrgan(
-			@RequestBody AuthenticatedRequest<RegisterOrganRequest> ar
-			) {
-		
-        this.authService.validRoleOrThrow(ar.authCredentials(), Role.COORDINATOR);
-		
+		@RequestBody @AuthenticatedRequestPayload(Role.COORDINATOR) AuthenticatedRequest<RegisterOrganRequest> ar
+	) {
 		return ResponseEntity.ok(organService.registerOrgan(ar));
 	}
 }
